bc4f583d47c804e1f3279d5ab9b5925afa888c35
lucascervera <lucas@lucascervera.com>
2021-09-23 12:54:22 +0200

    Added GTM code

--- components/PageHead.tsx
+++ components/PageHead.tsx
@@ -23,6 +23,17 @@ export const PageHead: React.FC<types.PageProps> = ({ site }) => {
 
       <meta name='theme-color' content='#EB625A' />
       <meta property='og:type' content='website' />
+
+      <!-- Global site tag (gtag.js) - Google Analytics -->
+      <script async src="https://www.googletagmanager.com/gtag/js?id=G-PQQY873JFC"></script>
+      <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-PQQY873JFC');
+      </script>
+      
     </Head>
   )
 }
