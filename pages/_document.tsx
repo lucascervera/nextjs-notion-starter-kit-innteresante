0c3496359f264dab7d0d23777b51b528b0e46e60
lucascervera <lucas@lucascervera.com>
2021-09-23 11:17:57 +0200

    Restored original file

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,14 +8,6 @@ export default class MyDocument extends Document {
       <IconContext.Provider value={{ style: { verticalAlign: 'middle' } }}>
         <Html lang='en'>
           <Head>
-
-            <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-5XPJF5');</script>
-
-            
             <link rel='shortcut icon' href='/favicon.png' />
 
             <link
@@ -46,10 +38,6 @@ export default class MyDocument extends Document {
           </Head>
 
           <body>
-
-            <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5XPJF5"
-            height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-
             <script src='noflash.js' />
 
             <Main />
