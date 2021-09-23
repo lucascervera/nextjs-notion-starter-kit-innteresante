9ffa53672ef240025f082bb03a16bd94e20df6d0
lucascervera <lucas@lucascervera.com>
2021-09-23 11:30:39 +0200

    Include GTM attempt

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -7,7 +7,28 @@ export default class MyDocument extends Document {
     return (
       <IconContext.Provider value={{ style: { verticalAlign: 'middle' } }}>
         <Html lang='en'>
-          <Head>
+          <Head>            
+          <>
+              {/* Google Tag Manager */}
+              <script
+                  dangerouslySetInnerHTML={{
+                      __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                          })(window,document,'script','dataLayer','${GTM-5XPJF5}');`,
+                  }}
+              />
+              {/* End Google Tag Manager */}
+              {/* Google Tag Manager (noscript) */}
+              <noscript
+                  dangerouslySetInnerHTML={{
+                      __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=${GTM-5XPJF5}" height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
+                  }}
+              />
+              {/* End Google Tag Manager (noscript) */}
+          </>
+            
             <link rel='shortcut icon' href='/favicon.png' />
 
             <link
