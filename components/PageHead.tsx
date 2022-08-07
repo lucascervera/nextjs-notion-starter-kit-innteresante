52f35b9811ab866d14520288b7dab2f939251c35
lucascervera <lucas@lucascervera.com>
2022-08-07 15:50:48 +0200

    Error fix attempt 4

--- components/PageHead.tsx
+++ components/PageHead.tsx
@@ -24,15 +24,22 @@ export const PageHead: React.FC<types.PageProps> = ({ site }) => {
       <meta name='theme-color' content='#EB625A' />
       <meta property='og:type' content='website' />
       
-      
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-KN64N6NYW9"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-KN64N6NYW9');
-</script>
+             <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=G-KN64N6NYW9`}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'UA-CODE', {
+              page_path: window.location.pathname,
+            });
+          `,
+            }}
+          />
       
     </Head>
   )
